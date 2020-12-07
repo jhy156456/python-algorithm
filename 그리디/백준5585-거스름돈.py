f4d7a23d9a4f185c92ea3a12b8a5dc3c2ae826ee
jhy156456 <gusdud0222@naver.com>
2020-12-07 22:40:30 +0900

    study

---
+++ 그리디/백준5585-거스름돈.py
@@ -0,0 +1,10 @@
+n = int(input())
+# n = map(int,input().split())
+n = 1000-n
+coin_types = [500, 100, 50, 10, 5, 1]
+count = 0
+for coin in coin_types:
+    count += n // coin
+    n = n % coin
+
+print(count)
---
+++ 그리디/이코테2-큰 수의 법칙.py
@@ -0,0 +1,22 @@
+n, m, k = map(int, input().split())
+data = list(map(int, input().split()))
+
+data.sort(reverse=True)
+
+print(data)
+i = 0
+result = 0
+originalK = k
+if data[0] != data[1]:
+    while i != m:
+        if k > 0:
+            result = result + data[0]
+        else:
+            result = result + data[1]
+            k = originalK
+        k = k - 1
+        i = i + 1
+else:
+    result = data[0] * m
+
+print(result)

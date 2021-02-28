6af25d4abbd3ab35e7f7aff59b3bc0af8eb1dde5
gusdud0222 <gusdud0222@daum.net>
2021-02-28 17:57:35 +0900

    study

---
+++ 06.정렬/삽입정렬.py
@@ -0,0 +1,15 @@
+numbers = list([7, 5, 9, 0, 3, 1, 6, 2, 4, 8])
+results = []
+results.append(numbers[0])
+
+for i in range(1, len(numbers)):
+    for j in range(i):
+        if numbers[i] < results[j]:
+            results.insert(j,numbers[i])
+            break
+        if j == i-1:
+            results.append(numbers[i])
+
+print(results)
+
+

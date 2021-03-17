62c5ea556a0b97bb5eafb42a49bfc6411a22df0d
jhy156456 <gusdud0222@naver.com>
2021-03-18 08:03:44 +0900

    study

---
+++ 07.이진탐색/이코테07-실전03,백준2805-떡복이 떡 만들기.py
@@ -0,0 +1,42 @@
+def binary_search(target, start, end):
+    center = (start + end) // 2
+    if start > end: return center
+    sum = getSumList(center)
+    # print("start",start,"end",end,"center",center,"sum",sum)
+    if sum < target:
+        return binary_search(target, start, center - 1)
+    elif sum > target:
+
+        return binary_search(target, center + 1, end)
+    # 일치하는경우
+    else:
+        return center
+
+
+def getSumList(minusValue):
+    sum = 0
+    for i in list:
+        if i > minusValue:
+            sum += i - minusValue
+    return sum
+
+
+# def getSumList(minusValue):
+#     sum = 0
+#     for i in list:
+#         sumValue = i-minusValue
+#         if sumValue > 0:
+#             sum = sum+ sumValue
+#     return sum
+
+n, m = map(int, input().split())
+list = list(map(int, input().split()))
+end = max(list)
+result = print(binary_search(m, 0, end))
+
+if result != None:
+    print(result + 1)
+
+
+
+

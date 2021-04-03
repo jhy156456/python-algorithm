390b6cc5f510d5158b223c0279921340aaf71226
jhy156456 <gusdud0222@naver.com>
2021-04-03 16:24:33 +0900

    study

--- 14.트리/tree(inorder,preorder,postorder).java
+++ 14.트리/tree(inorder,preorder,postorder).java
@@ -8,7 +8,7 @@ public class test {
         insert(root, 30);
         insert(root, 20);
         insert(root, 40);
-        insert(root, 70);
+        insert(root, 7 0);
         insert(root, 60);
         insert(root, 80);
 
